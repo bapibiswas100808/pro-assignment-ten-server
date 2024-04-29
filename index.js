3ea13b32e12ef9fd63ee5a3b3e6edca5546a6acc
bapibiswas100808 <star.bappy@gmail.com>
2024-04-29 14:33:17 +0600

    post method added

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const allCraftCollection = client.db("allCraftDB").collection("allCraft");
+    // post
+    app.post("/allCraft", async (req, res) => {
+      const newData = req.body;
+      console.log(newData);
+      const result = await allCraftCollection.insertOne(newData);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,7 +41,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
